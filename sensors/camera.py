49ff059fc7c24bd16048741648a1a2bdbae205c9
Milan Munko <milan.munko@ai-maps.com>
2017-07-07 11:22:48 +0200

    vyhodene preview

--- sensors/camera.py
+++ sensors/camera.py
@@ -5,7 +5,4 @@ import os
 camera = PiCamera()
 
 def takeSnap(filePath):
-    camera.start_preview()
-    sleep(5)
     camera.capture(os.path.join(filePath,strftime('%y%m%d-%H%M%S',localtime()),'.jpeg'))
-    camera.stop_preview()
