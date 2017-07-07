3da9650183c323f6dd52885c01f6f30592216067
Milan Munko <milan.munko@ai-maps.com>
2017-07-07 10:54:28 +0200

    fix

--- sensors/camera.py
+++ sensors/camera.py
@@ -7,5 +7,5 @@ camera = PiCamera()
 def takeSnap(filePath):
     camera.start_preview()
     sleep(5)
-    camera.capture(os.path.join(filePath,strftime(localtime())))
+    camera.capture(os.path.join(filePath,strftime('%y%m%d-%H%M%S',localtime()),'.jpeg'))
     camera.stop_preview()
